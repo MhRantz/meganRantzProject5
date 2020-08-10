46f21e298dec628e9445924a43587613fbad4ab4
MhRantz <meganrantz@gmail.com>
2020-08-10 16:41:47 -0400

    Updates

---
+++ precache-manifest.27cb005e8b1c6ce162c110d5254afab8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e09736d58a5c638dfaa35df34faea72",
+    "url": "/meganRantzProject5/index.html"
+  },
+  {
+    "revision": "8931f6f1eb9d0b19eb1c",
+    "url": "/meganRantzProject5/static/css/main.fc877719.chunk.css"
+  },
+  {
+    "revision": "7c8bc6b378cd8d5e9399",
+    "url": "/meganRantzProject5/static/js/2.6493abbe.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/meganRantzProject5/static/js/2.6493abbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8931f6f1eb9d0b19eb1c",
+    "url": "/meganRantzProject5/static/js/main.af524ce4.chunk.js"
+  },
+  {
+    "revision": "95cbc2cf43bbeac9347b",
+    "url": "/meganRantzProject5/static/js/runtime-main.a1e2914e.js"
+  }
+]);
