2e0763102ece3b038c5b64600e672cbfbd7b72fe
MhRantz <meganrantz@gmail.com>
2020-08-12 13:58:30 -0400

    cool stuff

---
+++ src/extra.js
@@ -0,0 +1,10 @@
+const listNames = [
+    {query:"hardcover-nonfiction", name:"Non-Fiction"},
+    {query:"young-adult", name:"Young Adult"},
+    {query:"hardcover-graphic-books", name:"Graphic Novels"},
+    {query:"food-and-fitness", name:"Food and Fitness"},
+    {query:"travel", name: "Travel"},
+    {query:"education", name:"Education"},
+    {query:"science", name:"Science"}
+]; 
+export default listNames;
